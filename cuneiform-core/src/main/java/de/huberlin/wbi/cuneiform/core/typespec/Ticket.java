f2e3717fae958e0c26c6726f069f89fb5c9d6e16
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-03-26 21:11:47 +0100

    More of the type spec added.

---
+++ cuneiform-core/src/main/java/de/huberlin/wbi/cuneiform/core/typespec/Ticket.java
@@ -0,0 +1,12 @@
+package de.huberlin.wbi.cuneiform.core.typespec;
+
+import de.huberlin.wbi.cuneiform.core.libfun.Record;
+
+public class Ticket extends Record {
+
+	private static final String SYMBOL_TICKET = "ticket";
+	
+	public Ticket() {
+		super( SYMBOL_TICKET );
+	}
+}
